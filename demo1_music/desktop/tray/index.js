c4edd375c942e72ebc726a6565c0987391c270f8
spiderT <1304126527@qq.com>
2020-02-21 19:41:47 +0800

    渲染进程和主进程通信

--- demo1_music/desktop/tray/index.js
+++ demo1_music/desktop/tray/index.js
@@ -12,7 +12,7 @@ let tray;
 
 function setTray() {
   tray = new Tray(path.resolve(__dirname, '../../resources/images/zhizhuxia_2.png'));
-  tray.on('click', show);
+  tray.on('click', console.log('click'));
   tray.on('right-click', () => {
     const contextMenu = Menu.buildFromTemplate([{
         label: '显示',
