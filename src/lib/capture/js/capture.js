5ddf9b87bcaacf6bbdbf16cc42a89fb6d0ea9005
spiderT <1304126527@qq.com>
2020-05-28 18:34:47 +0800

    截图功能fix

--- src/lib/capture/js/capture.js
+++ src/lib/capture/js/capture.js
@@ -14,21 +14,22 @@ const {
 
 
 desktopCapturer.getSources({
-    types: ['screen'],
+    types: ['screen', 'window'],
     thumbnailSize: {
         width,
         height
     }
-}, async (error, sources) => {
-    if (error) return console.log(error)
-    const screenImgUrl = sources[0].thumbnail.toDataURL()
+}).then(
+    async (sources) => {
+        const screenImgUrl = sources[0].thumbnail.toDataURL()
 
-    const bg = document.querySelector('.bg')
-    const rect = document.querySelector('.rect')
-    const sizeInfo = document.querySelector('.size-info')
-    const toolbar = document.querySelector('.toolbar')
-    const draw = new Draw(screenImgUrl, bg, width, height, rect, sizeInfo, toolbar)
-    document.addEventListener('mousedown', draw.startRect.bind(draw))
-    document.addEventListener('mousemove', draw.drawingRect.bind(draw))
-    document.addEventListener('mouseup', draw.endRect.bind(draw))
-})
+        const bg = document.querySelector('.bg')
+        const rect = document.querySelector('.rect')
+        const sizeInfo = document.querySelector('.size-info')
+        const toolbar = document.querySelector('.toolbar')
+        const draw = new Draw(screenImgUrl, bg, width, height, rect, sizeInfo, toolbar)
+        document.addEventListener('mousedown', draw.startRect.bind(draw))
+        document.addEventListener('mousemove', draw.drawingRect.bind(draw))
+        document.addEventListener('mouseup', draw.endRect.bind(draw))
+    }
+).catch(err => console.log('err', err))
